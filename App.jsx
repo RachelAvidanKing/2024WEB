09d2237fc04bfaa6837e08db01d4d8eeed6c5a00
Keren Kazatchinski <Kerenkaza29@gmail.com>
2025-01-03 11:50:14 +0200

    change neww

--- App.jsx
+++ App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import necessary components for routing
 import "./App.css"
 import "./index.css"; // Import global CSS for styling
-import Title from "./components/Title.jsx"; // Import the Title component for the header
-import LoginPage from "./components/index.jsx"; // Import the LoginPage component
-import SignUp from "./components/SignUp.jsx"; // Import the SignUp component
-import Options from "./components/Options.jsx"; // Import the Options component for user options
-import Questions from "./components/Questions.jsx"; // Import the Questions component for question suggestions
-import FullForm from "./components/FullForm.jsx"; // Import the FullForm component for interviewer form fill
-import History from "./components/History.jsx"; // Import the History component (ensure it's available)
-import { ThemeProvider } from "./components/ThemeContext.jsx";
+import Title from "./src/components/Title.jsx"; // Import the Title component for the header
+import LoginPage from "./src/components/LoginPage.jsx"; // Import the LoginPage component
+import SignUp from "./src/components/SignUp.jsx"; // Import the SignUp component
+import Options from "./src/components/Options.jsx"; // Import the Options component for user options
+import Questions from "./src/components/Questions.jsx"; // Import the Questions component for question suggestions
+import FullForm from "./src/components/FullForm.jsx"; // Import the FullForm component for interviewer form fill
+import History from "./src/components/History.jsx"; // Import the History component (ensure it's available)
+import { ThemeProvider } from "./src/components/ThemeContext.jsx";
 import { createContext } from "react";
 
 export const ThemeContext =createContext(null);

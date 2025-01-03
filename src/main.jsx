59b3f558c2ee6cc77760d2d5463d5a5943df3ffd
Keren Kazatchinski <Kerenkaza29@gmail.com>
2025-01-03 11:18:28 +0200

    change

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider> {/* Wrap App with ThemeProvider */}
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
